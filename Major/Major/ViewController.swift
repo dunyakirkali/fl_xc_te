8c12bae92fefc2463a2a54ffe33209583744b00a
Dunya Kirkali <dunyakirkali@gmail.com>
2019-05-01 20:15:03 +0200

    Add project to test major bump

---
+++ Major/Major/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Major
+//
+//  Created by Dunya Kirkali on 01/05/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
